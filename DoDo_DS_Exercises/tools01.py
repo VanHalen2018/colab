12bb483b450cdf448e1f56d46f351835f79dfeeb
Van Halen <35001009+VanHalen2018@users.noreply.github.com>
2020-12-26 10:31:25 +0100

    Create tools01.py
    # gat data # generate n "bad lines"

---
+++ DoDo_DS_Exercises/tools01.py
@@ -0,0 +1,34 @@
+# Tools for DoDo DS exercises
+# Write Python 3 code in this online editor and run it.
+
+import pandas as pd 
+import numpy as np
+
+filename_ = "https://raw.githubusercontent.com/2blam/ML/master/deep_learning/Churn_Modelling.csv"
+
+# gat data
+
+def getdata(fname = filename_):
+    '''
+    read the csv formated data from web
+    param fname: file name and html address - default filename_
+    index is the first column
+    return dataframe
+    '''
+    temp = pd.read_csv(fname, index_col=0)
+    return temp
+
+# generate n "bad lines"
+    
+ms_lines_ = 20
+def missing_lines(df, columns, n = ms_lines_):
+  '''
+  Creates n "bad rows" in the database
+  param df: dataframe
+  param columns: list of the bad columns in the datafarme - default ms_lines_
+  param n: count of the bad rows
+  '''
+  missing_index = np.random.randint(len(df), size=n)
+  df.loc[missing_index, columns] = np.nan
+  return df
+ 
